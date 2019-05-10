4187a241f4c2ccd4fd62070bbd54277c25a91f2d
Buchner72 <franz.buchner@gmx.at>
2019-05-10 09:22:25 +0200

    Lauffähig

--- WebAPI/Controllers/VTVPController.cs
+++ WebAPI/Controllers/VTVPController.cs
@@ -14,15 +14,17 @@ namespace WebAPI.Controllers
         List<Person> ListP = new List<Person>();
 
         // GET: api/VTVP
-        public List<Person> Get()
+        public List<Person> GetVP()
         {
             Person p1 = new Person();
             p1.Anrede = EnumData.Anrede.Herr;
-            p1.Titel = "Mag;";
+            p1.Titel = "Mag";
             p1.Vorname = "Robert";
             p1.Nachname = "Krallinger";
             p1.Geburtsdatum = "01.01.1980";
             p1.Alter = "39";  // In einer Funktion berechnen
+
+            
             ListP.Add(p1);
 
             Person p2 = new Person();
