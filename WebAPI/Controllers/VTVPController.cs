edef211490e05c1eccb4e66185045fdbca8a0fa9
Buchner72 <franz.buchner@gmx.at>
2019-05-10 09:03:43 +0200

    Add VertragsLibrary

---
+++ WebAPI/Controllers/VTVPController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VertragsLibrary;
+
+namespace WebAPI.Controllers
+{
+    public class VTVPController : ApiController
+    {
+
+        List<Person> ListP = new List<Person>();
+
+        // GET: api/VTVP
+        public List<Person> Get()
+        {
+            Person p1 = new Person();
+            p1.Anrede = EnumData.Anrede.Herr;
+            p1.Titel = "Mag;";
+            p1.Vorname = "Robert";
+            p1.Nachname = "Krallinger";
+            p1.Geburtsdatum = "01.01.1980";
+            p1.Alter = "39";  // In einer Funktion berechnen
+            ListP.Add(p1);
+
+            Person p2 = new Person();
+            p2.Anrede = EnumData.Anrede.Frau;
+            p2.Vorname = "Maria";
+            p2.Nachname = "Krallinger";
+            p2.Geburtsdatum = "23.05.1972";
+            p2.Alter = "47";  // In einer Funktion berechnen
+            ListP.Add(p2);
+
+            return ListP;      
+        }
+    }
+}
---
+++ WebAPI/Controllers/VertragController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VertragsLibrary;
+
+namespace WebAPI.Controllers
+{
+    public class VertragController : ApiController
+    {
+        Vertrag v1 = new Vertrag();
+
+        // GET: api/Vertrag
+        public Vertrag Get()
+        {
+            v1.Versicherungsnehmer.Vorname = "Franz";
+            v1.Versicherungsnehmer.Nachname = "Buchner";
+
+            //Versicherte Person hinzufügen
+            VersichertePerson p1 = new VersichertePerson();
+            p1.Vorname = "Versicherte Person Nr1";
+            v1.VersichertePersonen.Add(p1);
+
+            return v1;
+        }            
+    }
+}
